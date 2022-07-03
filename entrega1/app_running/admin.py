5a1383cea8baec3f5c608db0ccd4ba233f4e2797
Lorena <lemilse@yahoo.com.ar>
2022-07-03 15:24:20 -0300

    segunda version

---
+++ entrega1/app_running/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(Corredor)
+admin.site.register(Carreras)
+admin.site.register(Teams)
+admin.site.register(Avatar)
+
+# Register your models here.
---
+++ entrega1/app_running/models.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from django.db import models
+from django.forms import DateField, EmailField
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Corredor (models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    modalidad = models.CharField(max_length=10)
+    email = models.EmailField()
+    team = models.CharField(max_length=10)
+    def __str__ (self):
+        return f"Nombre:{self.nombre} - Apellido:{self.apellido} - Modalidad:{self.modalidad} - Email:{self.email} - Team:{self.team}"
+
+
+class Carreras (models.Model):
+    nombre = models.CharField(max_length=20)
+    modalidad = models.CharField(max_length=10)
+    distancia= models.IntegerField()
+    fecha = models.DateField()
+    def __str__ (self):
+        return f"Nombre:{self.nombre} - Modalidad:{self.modalidad} - Distancia:{self.distancia} - Fecha:{self.fecha}"
+
+class Teams (models.Model):
+    nombre = models.CharField(max_length=20)
+    modalidad = models.CharField(max_length=10)
+    email = models.EmailField()
+    def __str__ (self):
+        return f"Nombre:{self.nombre} - Modalidad:{self.modalidad} - Email:{self.email}"
+# Create your models here.
+class Avatar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='avatares' , null=True , blank=True)
+    
+
